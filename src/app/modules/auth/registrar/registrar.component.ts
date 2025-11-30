bbd4c0706648c24eb917bff992f1b8007c81114f
LizParadigm <jesss635000@gmail.com>
2025-11-30 13:51:25 -0600

    header creado

---
+++ src/app/modules/auth/registrar/registrar.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from '../../../shared/component/header/header/header.component';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-registrar',
+  standalone: true,
+  imports: [HeaderComponent],
+  templateUrl: './registrar.component.html',
+  styleUrl: './registrar.component.css'
+})
+export class RegistrarComponent {
+
+}
--- src/app/shared/services/rut/rut.service.ts
+++ src/app/shared/services/rut/rut.service.ts
@@ -13,5 +13,25 @@ export class RutService {
   home() {
     this.router.navigateByUrl('/home');
   }
+  controlHeader() {
+    // decide si se muestra o no el header dependiendo la ruta actual y define si el usuario esta logueado o no dependiendo del contenido del localstorage
+    let version = 3; // version para header registrar false, version para header interior true. null es igual a inisicar sesion y recuperar
+    let usuario = false;//si hay algo en el storagelocal entonces se muestran los links interiores de la app web, si no hay nada entonces los de usuario no registrado, osea registrar
+    //retornamos mostrar: true or false. permisos storage true or false
+    switch (this.router.url) {
+      case '/home':
+      case '/historial':
+      case '/resultado':
+        version = 1;
+        break;
+      case '/registrar':
+        version = 2;
+        break;
+      default:
+        version = 3;
+        break;
+    }
+    return version;
+  }
 
 }
