5e5678ba9e599e1c0239a5b14dbba04ee5d71043
LizParadigm <jesss635000@gmail.com>
2025-11-30 14:40:55 -0600

    registrar creado, falta funcioin y css

--- src/app/modules/auth/registrar/registrar.component.ts
+++ src/app/modules/auth/registrar/registrar.component.ts
@@ -1,14 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HeaderComponent } from '../../../shared/component/header/header/header.component';
 import { RouterLink } from '@angular/router';
+import { FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-registrar',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent,
+    ReactiveFormsModule,
+    CommonModule, RouterLink],
   templateUrl: './registrar.component.html',
   styleUrl: './registrar.component.css'
 })
-export class RegistrarComponent {
+export class RegistrarComponent implements OnInit {
+  errorNombre!: boolean;
+  errorApellidoP!: boolean;
+  errorApellidoM!: boolean;
+  errorCorreo!: boolean;
+  errorContrasena!: boolean;
+  errorRContrasena!: boolean;
+  formulario!: FormGroup;
+  ngOnInit(): void {
+  }
+
+  registrar() {
+
+  }
 
 }
