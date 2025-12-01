43d2cd704314a279853a593ba7b7adc1d1339aaa
LizParadigm <jesss635000@gmail.com>
2025-12-01 13:49:24 -0600

    inicio conectado a consulta y los demas

--- src/app/shared/services/rut/rut.service.ts
+++ src/app/shared/services/rut/rut.service.ts
@@ -14,14 +14,14 @@ export class RutService {
     this.router.navigateByUrl('/home');
   }
   controlHeader() {
-    // decide si se muestra o no el header dependiendo la ruta actual y define si el usuario esta logueado o no dependiendo del contenido del localstorage
-    let version = 3; // version para header registrar false, version para header interior true. null es igual a inisicar sesion y recuperar
-    let usuario = false;//si hay algo en el storagelocal entonces se muestran los links interiores de la app web, si no hay nada entonces los de usuario no registrado, osea registrar
-    //retornamos mostrar: true or false. permisos storage true or false
+
+    let version = 3;
+
     switch (this.router.url) {
       case '/home':
-      case '/historial':
-      case '/resultado':
+      case '/home/historial':
+      case '/home/consulta':
+      case '/home/resultado':
         version = 1;
         break;
       case '/registrar':
@@ -35,7 +35,7 @@ export class RutService {
   }
 
   consulta() {
-    this.router.navigateByUrl('/consulta');
+    this.router.navigateByUrl('/home/consulta');
   }
 
 }
