91e6158baf6ac673d6b0ddf0b05eb91f049f9964
LizParadigm <jesss635000@gmail.com>
2025-11-30 22:33:43 -0600

    estilos actualizados

--- src/app/modules/auth/registrar/registrar.component.ts
+++ src/app/modules/auth/registrar/registrar.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HeaderComponent } from '../../../shared/component/header/header/header.component';
 import { RouterLink } from '@angular/router';
-import { FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -21,8 +21,40 @@ export class RegistrarComponent implements OnInit {
   errorContrasena!: boolean;
   errorRContrasena!: boolean;
   formulario!: FormGroup;
+  constructor(
+    private fb: FormBuilder
+  ) { };
   ngOnInit(): void {
+    this.formulario = this.fb.group({
+      nombre: ['', [
+        Validators.required,
+        Validators.pattern(/^[a-zA-Z\s]+$/),
+        Validators.minLength(3),
+      ]],
+
+      apellidoP: ['', [
+        Validators.required,
+        Validators.pattern(/^[a-zA-Z\s]+$/),
+        Validators.minLength(3),
+      ]],
+
+      apellidoM: ['', [
+        Validators.required,
+        Validators.pattern(/^[a-zA-Z\s]+$/),
+        Validators.minLength(3),
+      ]],
+      correo: ['', [
+        Validators.required,
+        Validators.email,
+      ]],
+      contrasena: ['', [
+        Validators.required,
+        Validators.minLength(8)
+      ]]
+    })
   }
+
+
 
   registrar() {
 
